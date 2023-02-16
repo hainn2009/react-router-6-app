0a2afee4cc97bcd21ea0412f1e719a37b0018690
Nguyen Ngoc Hai <hainn2009@gmail.com>
2023-02-16 09:30:55 +0700

    update

--- src/routes/destroy.jsx
+++ src/routes/destroy.jsx
@@ -2,7 +2,7 @@ import { redirect } from "react-router-dom";
 import { deleteContact } from "../contact";
 
 export async function action({ params }) {
-    throw new Error("error")
+    // throw new Error("error")
     await deleteContact(params.contactId);
     return redirect("/");
 }
