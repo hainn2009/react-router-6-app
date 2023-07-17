baf35ae7a718b037424b514d96bd96357925dc41
Nguyen Ngoc Hai <hainn2009@gmail.com>
2023-07-17 15:52:21 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -35,7 +35,7 @@ const router = createBrowserRouter([
                     },
                     {
                         path: "contacts/:contactId/destroy",
-                        element: <h1>Hello123</h1>,
+                        element: <h1>Hello1234</h1>,
                         action: destroyContactAction,
                         errorElement: <div>There was an error</div>,
                     },
